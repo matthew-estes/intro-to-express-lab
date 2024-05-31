e9bb7a72cff6747f24fffed4a2de8420916f421c
matthew-estes <matthew.estes19@gmail.com>
2024-05-31 17:38:52 -0400

    completed #4

--- server.js
+++ server.js
@@ -34,10 +34,41 @@ app.get("/collectibles/:index", function (req, res) {
 
   const index = req.params.index;
   if (index >= 0 && index < collectibles.length) {
-    res.send(`So, you want the ${collectibles[index].name}? For $${collectibles[index].price}, it can be yours!`);
-    } else { 
-      res.send("This item is not yet in stock. Check back soon!");
-    };
-  });
+    res.send(
+      `So, you want the ${collectibles[index].name}? For $${collectibles[index].price}, it can be yours!`
+    );
+  } else {
+    res.send("This item is not yet in stock. Check back soon!");
+  }
+});
 
+//4. Filter Shoes by Query Parameters
 
+app.get("/shoes", function (req, res) {
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" },
+  ];
+
+  let filteredShoes = [...shoes];
+
+  const minPrice = req.query["min-price"];
+  const maxPrice = req.query["max-price"];
+  const type = req.query.type;
+
+  if (!isNaN(minPrice)) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.price >= minPrice);
+  }
+  if (!isNaN(maxPrice)) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.price <= maxPrice);
+  }
+  if (type) {
+    filteredShoes = filteredShoes.filter((shoe) => shoe.type === type);
+  }
+  res.send(filteredShoes);
+});
